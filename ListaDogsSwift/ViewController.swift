2dd3d54786db47b15a5e3e6cd33ebfea7343e4e9
Matheus Pereira <matheuscelsopereira@gmail.com>
2020-02-28 11:51:40 -0300

    Layout Favorito cachorro

--- ListaDogsSwift/ViewController.swift
+++ ListaDogsSwift/ViewController.swift
@@ -10,7 +10,13 @@ import UIKit
 import Alamofire
 
 
-class ViewController: UIViewController {
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate {
+    
+    @IBOutlet weak var tableViewRaca: UITableView!
+    @IBOutlet weak var Image: UIImageView!
+   
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
