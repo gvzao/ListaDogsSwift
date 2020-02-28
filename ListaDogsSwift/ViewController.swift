e1575fd231599580cc2079cacd88ef2b9a288bdb
Jonathan <jonathan@Jonathans-iMac.local>
2020-02-28 08:30:53 -0300

    Initial Commit

---
+++ ListaDogsSwift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ListaDogsSwift
+//
+//  Created by Jonathan on 28/02/20.
+//  Copyright © 2020 hbsoftware.ios. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
