1e37fbf0d85316ba2532ba7415d436a335c82fad
Jonathan <jonathan@Jonathans-iMac.local>
2020-03-02 11:43:44 -0300

    fazendo os tableViewCell e  manipulação da tabela, adicionei a imagem  e botão na view da tabela.

---
+++ ListaDogsSwift/Favorito.swift
@@ -0,0 +1,21 @@
+//
+//  Favorito.swift
+//  ListaDogsSwift
+//
+//  Created by Jonathan on 02/03/20.
+//  Copyright © 2020 hbsoftware.ios. All rights reserved.
+//
+
+import UIKit
+
+class Favorito: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
