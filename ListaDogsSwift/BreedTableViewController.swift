5fed6265ac49c86eeca4e1b910177ec6acda86ba
alan-oliveirafer <58215765+alan-oliveirafer@users.noreply.github.com>
2020-03-05 10:11:43 -0300

    Bro, I've created that class that you should use to save the fuckin' breeds, 'kay? NEWS2

---
+++ ListaDogsSwift/BreedTableViewController.swift
@@ -0,0 +1,19 @@
+//
+//  BreedTableViewController.swift
+//  ListaDogsSwift
+//
+//  Created by Jonathan on 05/03/20.
+//  Copyright © 2020 hbsoftware.ios. All rights reserved.
+//
+
+import UIKit
+import os.log
+
+class BreedTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        }
+
+}
